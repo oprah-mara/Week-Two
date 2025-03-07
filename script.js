27d0fd1ea75e7e1448b521f476d78c2ab7915e0f
https://github.com/oprah-mara <oparaamara997@gmail.com>
2025-03-06 22:52:11 -0500

    learnt prompt and alerts

---
+++ script.js
@@ -0,0 +1,6 @@
+// prompts and alerts
+let homeTown = prompt("What is your hometown?");
+alert(homeTown);
+let firstName = prompt("What is your first name?");
+let lastName = prompt ("What is your last name?");
+alert(firstName + " " + lastName);
